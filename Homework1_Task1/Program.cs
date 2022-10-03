048bd1d16e76f8ebd2a0e5b90cec50d365df2f22
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-10-03 19:58:47 +0300

    feat: add unit test to sort function fix: rename function corresponding to code style

--- Homework1_Task1/Program.cs
+++ Homework1_Task1/Program.cs
@@ -1,23 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Homework1_Task1;
 
-static void sortArray(int[] array)
+public static class Program
 {
-    for (var i = 0; i < array.Length; i++)
-    for (var j = i; j > 0 && array[j] < array[j - 1]; j--)
-        (array[j], array[j - 1]) = (array[j - 1], array[j]);
-}
+    public static void SortArray(int[] array)
+    {
+        for (var i = 0; i < array.Length; i++)
+        for (var j = i; j > 0 && array[j] < array[j - 1]; j--)
+            (array[j], array[j - 1]) = (array[j - 1], array[j]);
+    }
 
-Console.Write("Write size of array: ");
-var size = Convert.ToInt32(Console.ReadLine());
-while (size < 1)
-{
-    Console.Write("Size of array must be positive, try again: ");
-    size = Convert.ToInt32(Console.ReadLine());
-}
+    private static void Main()
+    {
+        Console.Write("Write size of array: ");
 
-Console.WriteLine("Write elements of array one in a line: ");
-var array = new int[size];
-for (var i = 0; i < size; i++) array[i] = Convert.ToInt32(Console.ReadLine());
-sortArray(array);
-Console.WriteLine("Sorted array:");
-foreach (var t in array) Console.WriteLine(t);
+        var size = Convert.ToInt32(Console.ReadLine());
+        while (size < 1)
+        {
+            Console.Write("Size of array must be positive, try again: ");
+            size = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Write elements of array one in a line: ");
+
+        var array = new int[size];
+        for (var i = 0;
+             i < size;
+             i++) array[i] = Convert.ToInt32(Console.ReadLine());
+        SortArray(array);
+        Console.WriteLine("Sorted array:");
+        foreach (var t in array) Console.WriteLine(t);
+    }
+}
