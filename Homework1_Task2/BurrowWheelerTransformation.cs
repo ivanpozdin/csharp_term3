be2dc8696d2d6c1a963152b25372bbeea970f66c
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-12-04 00:21:21 +0300

    fix: sorting problem in BWT feat: make 3 unit tests

--- Homework1_Task2/BurrowWheelerTransformation.cs
+++ Homework1_Task2/BurrowWheelerTransformation.cs
@@ -8,61 +8,38 @@ public class BurrowWheelerTransformation
         return str.Remove(0, 1) + startElement;
     }
 
-    private static void InputStringToList(string str, List<string> list)
-    {
-        for (int i = 0; i < list.Count; i++)
-        {
-            if (String.CompareOrdinal(str, list[i]) == -1)
-            {
-                list.Insert(i, str);
-                return;
-            }
-        }
-        list.Add(str);
-    }
-
     public static KeyValuePair<string, int> DoBurrowsWheelerTransformation(string str)
     {
         var listOfStringRotations = new List<string>();
         var newStr = string.Copy(str);
-        for (int i = 0; i < str.Length; i++)
+        for (var i = 0; i < str.Length; i++)
         {
             newStr = GetNextStringRotation(newStr);
-            Console.WriteLine(newStr);
-            InputStringToList(string.Copy(newStr), listOfStringRotations);
+            listOfStringRotations.Add(newStr);
         }
+
+        listOfStringRotations = listOfStringRotations.OrderBy(q => q).ToList();
         newStr = string.Empty;
         var strIndex = 0;
         foreach (var stringRotation in listOfStringRotations)
         {
             newStr += stringRotation[^1];
-            if (String.CompareOrdinal(str, stringRotation) == 0)
-            {
-                strIndex = newStr.Length - 1;
-            }
-            
+            if (string.CompareOrdinal(str, stringRotation) == 0) strIndex = newStr.Length - 1;
         }
 
         return new KeyValuePair<string, int>(newStr, strIndex);
     }
 
-    
-    
     public static string ReverseBurrowsWheelerTransformation(string strBWT, int index)
     {
+        if (strBWT == string.Empty) return string.Empty;
         var listOfStringRotations = new List<string>();
-        foreach (var letter in strBWT)
-        {
-            listOfStringRotations.Add(letter.ToString());
-        }
+        foreach (var letter in strBWT) listOfStringRotations.Add(letter.ToString());
         listOfStringRotations.Sort();
 
         while (listOfStringRotations[0].Length < strBWT.Length)
         {
-            for (int i = 0; i < strBWT.Length; i++)
-            {
-                listOfStringRotations[i] = strBWT[i] + listOfStringRotations[i];
-            }
+            for (var i = 0; i < strBWT.Length; i++) listOfStringRotations[i] = strBWT[i] + listOfStringRotations[i];
             listOfStringRotations.Sort();
         }
 
--- Homework1_Task2/Program.cs
+++ Homework1_Task2/Program.cs
@@ -6,13 +6,11 @@ public static class Program
     {
         Console.WriteLine("Enter string:");
         var input = Console.ReadLine();
-        while (input == null)
-        {
-            input = Console.ReadLine();
-        }
+        while (input == null) input = Console.ReadLine();
         var result = BurrowWheelerTransformation.DoBurrowsWheelerTransformation(input);
         Console.WriteLine(result.Key);
         Console.Write(result.Value);
+        Console.WriteLine();
         Console.WriteLine(BurrowWheelerTransformation.ReverseBurrowsWheelerTransformation(result.Key, result.Value));
     }
 }
