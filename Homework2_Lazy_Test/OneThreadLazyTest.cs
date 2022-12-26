8095c5c84b98fc20e80fc1af13501656c1c4fc32
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-12-26 03:14:52 +0300

    fix: add csproj files to git

---
+++ Homework2_Lazy_Test/OneThreadLazyTest.cs
@@ -0,0 +1,23 @@
+using Homework2_Lazy;
+
+namespace Homework2_Lazy_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        OneThreadLazy<int> lazy = new OneThreadLazy<int>(() => 100);
+        Assert.Equal(100, lazy.Get());
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var random = new Random();
+        OneThreadLazy<int> lazy = new OneThreadLazy<int>(() => random.Next(0, 10000));
+        var firstCall = lazy.Get();
+        var secondCall = lazy.Get();
+        Assert.Equal(firstCall, secondCall);
+    }
+}
