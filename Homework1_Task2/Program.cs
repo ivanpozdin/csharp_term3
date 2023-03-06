d9a5390635de1e45b64d2a4a66600959bbb5b9d0
Ivan Pozdin <ivanllpozdin@gmail.com>
2023-03-06 12:43:00 +0300

    fix: do clean up code

--- Homework1_Task2/Program.cs
+++ Homework1_Task2/Program.cs
@@ -5,13 +5,12 @@ public static class Program
     private static void Main()
     {
         Console.WriteLine("Enter string:");
-        var input = Console.ReadLine();
-        while (input == null) input = Console.ReadLine();
-        var result = BurrowWheelerTransformation.DoBurrowsWheelerTransformation(input);
+        var text = Console.ReadLine();
+        while (text == null) text = Console.ReadLine();
+        var result = BurrowWheelerTransformation.DoBurrowsWheelerTransformation(text);
         Console.WriteLine(result.Key);
         Console.Write(result.Value);
         Console.WriteLine();
         Console.WriteLine(BurrowWheelerTransformation.ReverseBurrowsWheelerTransformation(result.Key, result.Value));
-        
     }
 }
--- Homework1_Task2/SuffixArray.cs
+++ Homework1_Task2/SuffixArray.cs
@@ -2,20 +2,16 @@ namespace Homework1_Task2;
 
 public static class SuffixArray
 {
-    public static int[] CreateSuffixArray(string s)
+    public static int[] CreateSuffixArray(string text)
     {
-        var n = s.Length;
-        var (positions, arrayForFastCompare) = GetStartingArraysPositionsAndArrayForFastCompare(s);
+        var n = text.Length;
+        var (positions, arrayForFastCompare) = GetStartingArraysPositionsAndArrayForFastCompare(text);
 
         for (var h = 0; 1 << h < n; ++h)
         {
             var pairs = new (int, int)[n];
-            var newPositions = new int[n];
-            for (var i = 0; i < n; i++)
-            {
-                pairs[i] = (arrayForFastCompare[i], arrayForFastCompare[(i + (1 << h)) % n]);
-                newPositions[i] = i;
-            }
+            var newPositions = Enumerable.Range(0, text.Length).ToArray();
+            for (var i = 0; i < n; i++) pairs[i] = (arrayForFastCompare[i], arrayForFastCompare[(i + (1 << h)) % n]);
 
             Array.Sort(pairs, newPositions);
             positions = newPositions;
@@ -28,7 +24,7 @@ public static class SuffixArray
                 arrayForFastCompare[positions[i]] = rating;
             }
 
-            if (rating == n-1)
+            if (rating == n - 1)
                 break;
         }
 
@@ -37,16 +33,14 @@ public static class SuffixArray
 
     private static (int[], int[]) GetStartingArraysPositionsAndArrayForFastCompare(string text)
     {
-        var n = text.Length;
         var chars = text.ToCharArray();
-        var positions = new int[n];
-        for (var i = 0; i < n; i++) positions[i] = i;
+        var positions = Enumerable.Range(0, text.Length).ToArray();
         Array.Sort(chars, positions);
-        var arrayForFastCompare = new int[n];
+        var arrayForFastCompare = new int[text.Length];
 
         var rating = 0;
         arrayForFastCompare[positions[0]] = rating;
-        for (var i = 1; i < n; i++)
+        for (var i = 1; i < text.Length; i++)
         {
             if (chars[i] != chars[i - 1]) rating++;
             arrayForFastCompare[positions[i]] = rating;
